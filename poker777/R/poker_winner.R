60f8572a878f0db5d50167de6e1a9927f9785895
hbsjain <126732252+hbsjain@users.noreply.github.com>
2024-03-19 17:41:53 +1100

    validating user input against acceptable format

--- poker777/R/poker_winner.R
+++ poker777/R/poker_winner.R
@@ -12,21 +12,34 @@ poker_winner <- function() {
   user_input <- function() {
     hands <- list()
 
+    card_values <- c("A","2","3","4","5","6","7","8","9","T","J","Q","K")
+    suits <- c("H", "C", "D", "S")
+
     for (i in 1:2) {
       hand <- character(0)
 
       while (length(hand) < 5) {
-        card <- readline(prompt = paste("Player", i, "- Enter a card (e.g., '2H' for 2 of hearts): "))
+        card <- casefold(readline(prompt = paste("Player", i, "- Enter a card (e.g., '2H' for 2 of hearts): ")),upper = TRUE)
         if (length(card) == 0) {
           print("Please enter a valid card.")
         } else {
-          hand <- c(hand, card)
+          # Extract card value and suit
+          card_value <- substr(card, 1, 1)
+          suit <- substr(card, 2, 2)
+
+          # Check if card value and suit are valid
+          if (card_value %in% card_values && suit %in% suits) {
+            hand <- c(hand, card)
+          } else {
+            print("Invalid card format. Please enter cards in the format '2H' for 2 of hearts.")
+          }
         }
       }
       hands[[i]] <- hand
     }
     return(hands)
   }
+
 
   hands <- user_input()
 
