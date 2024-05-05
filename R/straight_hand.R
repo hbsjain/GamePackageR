a561e12108b9eb0e36a8cac5fe8e03530904f7d4
hbsjain <126732252+hbsjain@users.noreply.github.com>
2024-05-05 14:23:48 +1000

    handling for A,2,3,4,5 straight hand

--- R/straight_hand.R
+++ R/straight_hand.R
@@ -1,5 +1,8 @@
 # Function to check if a hand is a straight
 is_straight <- function(hand) {
   hand_numeric <- sort(sapply(hand, convert_to_numeric))
+  if(identical(hand_numeric, c(2, 3, 4, 5, 13))){
+    return(TRUE)
+  }
   return(all(diff(hand_numeric) == 1))
 }
