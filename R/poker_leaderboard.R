174628dbff3f93494fb07727242d540b310c13bc
hbsjain <126732252+hbsjain@users.noreply.github.com>
2024-05-11 07:23:31 +1000

    Included new imports. exported vector and related methods, documented all functions

--- R/poker_leaderboard.R
+++ R/poker_leaderboard.R
@@ -1,3 +1,13 @@
+#' Leaderboard Generator. Based on the winners of each round and pot amount, arranges player positions and shows net amount won and rounds won by each player
+#'
+#' @param scoreboard A vector of type scores having the player no, pot amounts and bet amounts
+#'
+#' @import dplyr
+#' @export
+#'
+#' @examples
+#' pot_wins <- create_scoreboard(c(1,2,3), c(5000,3000,4000), c(200,500,300))
+#' poker_leaderboard(pot_wins)
 poker_leaderboard <- function(scoreboard) {
   scoreboard <- vec_cast.data.frame.scores(scoreboard)
   scoreboard %>%
@@ -8,8 +18,19 @@ poker_leaderboard <- function(scoreboard) {
     arrange(-actual_win)
 }
 
-poker_leaderboard(pot_wins)
 
+#' Used to create a Scoreboard vector which consists of winner, pot amount, bet amount for each round
+#'
+#' @param player_no A numeric vector having the player numbers
+#' @param pot A numeric vector having the pot amounts
+#' @param bet A numeric vector bet amounts which can't be more than pot amount
+#'
+#' @import vctrs
+#' @return scoreboard A custom vector usable to calculate leaderboard
+#' @export
+#'
+#' @examples
+#' create_scoreboard(c(1,2,3), c(5000,3000,4000), c(200,500,300))
 create_scoreboard <- function(player_no, pot, bet) {
   # Check if all parameters are of the same length
   if (!(length(player_no) == length(pot) &&
@@ -34,6 +55,15 @@ create_scoreboard <- function(player_no, pot, bet) {
   return(scoreboard)
 }
 
+
+
+#' Custom Formatting of the scores class
+#'
+#' @param x A scores vector rcrd class
+#' @param ... Other parameters if required
+#' @import vctrs
+#' @return Neatly prints the scores board
+#' @export
 format.scores <- function(x, ...) {
   cat(sprintf("Player    | Pot        | Bet \n"))
   cat(sprintf(
@@ -44,6 +74,14 @@ format.scores <- function(x, ...) {
   ))
 }
 
+#' Custom method for casting the scores class to data frame class
+#'
+#' @param x A scores vector rcrd class
+#' @param to A class that we need to convert to. By default a data frame
+#' @param ... Other parameters if required
+#' @import vctrs
+#' @return A data frame
+#' @export
 vec_cast.data.frame.scores <- function(x, to, ...){
   vctrs::vec_data(x)
 }
